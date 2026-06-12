29b1ae2fcde2ca20b8425c26d267b0ae2b31691d
mloilpy-ux <mloilpy@gmail.com>
2026-06-13 00:16:30 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        google()
+        google() // КРИТИЧЕСКИ ВАЖНО
         mavenCentral()
     }
 }
